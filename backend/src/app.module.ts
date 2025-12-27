bff0fadb54f1757c3f8f6757f4433afe1ba40f67
btrannn <le.btrann@gmail.com>
2025-12-27 21:43:14 +0700

    Modify BE to match with FE

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -4,9 +4,15 @@ import { ConfigModule, ConfigService } from '@nestjs/config'
 import { AdaptersModule } from './adopters/adopters.module';
 import { CatsModule } from './cats/cats.module';
 import { AdoptionsModule } from './adoptions/adoptions.module';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 @Module({
   imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', '..', 'public'), 
+      exclude: ['/api/(.*)'], 
+    }),
     ConfigModule.forRoot({
       isGlobal: true,
       envFilePath: ".env",
